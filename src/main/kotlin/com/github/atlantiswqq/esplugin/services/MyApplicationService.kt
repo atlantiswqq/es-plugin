f227141fd6bfb2bc2399146ed2778c91d3fe1dac
GitHub Action <action@github.com>
2022-03-09 03:53:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/atlantiswqq/esplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/atlantiswqq/esplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.atlantiswqq.esplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.atlantiswqq.esplugin.MyBundle
 
 class MyApplicationService {
 
